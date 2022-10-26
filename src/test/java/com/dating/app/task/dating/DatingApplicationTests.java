7520d2d91879dc89c1f30f698c0fbee2ed66770c
DarkLinkRT <48743585+DarkLinkRT@users.noreply.github.com>
2022-10-26 18:49:06 -0500

    Cretae new project in JAVA

---
+++ src/test/java/com/dating/app/task/dating/DatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dating.app.task.dating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
